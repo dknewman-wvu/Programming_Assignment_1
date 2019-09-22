c34b45f01b7dea2e3b0106f2c8ac45a4c7e9d3d9
dknewman-wvu <dknewman@mix.wvu.edu>
2019-09-22 15:12:22 -0400

    init

--- Programming_Assignment_1/Program.cs
+++ Programming_Assignment_1/Program.cs
@@ -6,7 +6,7 @@ namespace Programming_Assignment_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); //hello world
         }
     }
 }
