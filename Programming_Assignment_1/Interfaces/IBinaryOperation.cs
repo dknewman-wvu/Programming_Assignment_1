a599088ac0b6724c1b64a8372f89ff5416dc771a
dknewman-wvu <55037826+dknewman-wvu@users.noreply.github.com>
2019-09-22 19:55:59 -0400

    Delete IBinaryOperation.cs

--- Programming_Assignment_1/Interfaces/IBinaryOperation.cs
+++
@@ -1,9 +0,0 @@
-﻿interface IBinaryOperation
-{
-    string Description { get; }
-    string Operator { get; }
-    bool CanCalculate(string binaryOperator);
-    double Calculate(double x, double y);
-    double SqrtCalculate(double x);
-
-}
